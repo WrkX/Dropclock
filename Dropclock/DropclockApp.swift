21983a462daf829b19cfd9b814957fbe7aa8737e
WrkX <pankratz.jonas95@gmail.com>
2025-02-21 23:15:50 +0100

    Initial Commit

---
+++ Dropclock/DropclockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropclockApp.swift
+//  Dropclock
+//
+//  Created by Jonas Pankratz on 21.02.25.
+//
+
+import SwiftUI
+
+@main
+struct DropclockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
